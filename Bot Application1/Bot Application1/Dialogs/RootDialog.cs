90aa9fda32d254e245d8124e83a67782d62d7526
youngjuleenz <ylee447@aucklanduni.ac.nz>
2017-11-26 17:36:54 +1300

    cloned bot project into folder

---
+++ Bot Application1/Bot Application1/Dialogs/RootDialog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace Bot_Application1.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        protected string id { get; set; }
+
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            await context.PostAsync("Welcome to Contoso Bank, what is your ID?");
+            context.Wait(IdReceived);
+            //var activity = await result as Activity;
+
+            // calculate something for us to return
+            //int length = (activity.Text ?? string.Empty).Length;
+
+            // return our reply to the user
+            //await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            //context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task IdReceived(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            this.id = (await argument).Text; 
+            await context.PostAsync($"Hello {id}, How can I help you? 1. Exchange rates 2. Stock prices");
+        }
+    }
+}
